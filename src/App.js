d1cc49be20800588a4be5630b1dfa59688e1857d
Rebecca Ross <b.laurenross@gmail.com>
2021-02-17 19:54:31 +0000

    added api keys

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_ID}`;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React</h1>
+
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      {/* <button
+        onClick={() => {
+          console.log(process.env.REACT_APP_APP_ID);
+          console.log(process.env.REACT_APP_APP_ID);
+        }}
+      >
+        Click
+      </button> */}
     </div>
   );
 }
